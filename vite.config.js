8521c47d23d5493e884f454f68217a212d5aef0f
Afonso Pereira <afonsopinapereira@gmail.com>
2023-04-17 19:30:56 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
-import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-    plugins: [react(), svgrPlugin()],
+    plugins: [react()],
     server: {
         host: '0.0.0.0',
         port: 8886
     },
-    mainFields: [],
-    optimizeDeps: {
-        esbuildOptions: {
-            plugins: [esbuildCommonjs(['react-moment'])]
-        }
+    resolve: {
+        mainFields: []
     }
 });
